b445acb69a72ef7ebd6c853c970bfe5018c45f97
Jortizuwu <joseher2511@gmail.com>
2023-03-20 20:29:04 -0500

    change values in newData.json

---
+++ uwu.sql
@@ -0,0 +1,31 @@
+	
+
+
+    'Derecho'
+    'Administracion en Finanzas y Negocios Internacionales'
+    'Ingeniería Ambiental'
+    'Ingeniería de Sistemas'
+    'Ingeniería Industrial'
+    'Ingeniería Mecánica'
+    'Ingeniería Agronómica'
+    'Ingeniería de Alimentos'
+    'Medicina Veterinaria y Zootecnia' sindatos
+    'Bacteriologia'
+    'Enfermería'
+    'Tecnologia en Regencia y Farmacia'
+    'Administración en Salud'
+    'Estadistica' sindatos
+    'Matemáticas'
+    'Geografía'
+    'Física'
+    'Química'
+    'Biología'
+    'Licenciatura en Educacion Artistica'
+    'Licenciatura en Ciencias Sociales'
+    'Licenciatura Educacion Fisica, Recreacio y Deporte'
+    'Licenciatura en Literatura y Lengua Castellana'
+    'Licenciatura en informática'
+    'Licenciatura en Lengua Extrangera con Enfasis en Ingles'
+    'Licenciatura en Ciencias Naturales y Educacion Ambiental'
+    'Licenciatura en educación infantil'
+    'Acuicultura' sindatos
