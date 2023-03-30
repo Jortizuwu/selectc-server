d0272988a4375c33fbf2982007027f3e0d7b73d2
Jortizuwu <joseher2511@gmail.com>
2023-03-30 09:45:41 -0500

    trailingComma prettier rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,24 @@ module.exports = {
     node: true,
     // browser: true,
     commonjs: true,
-    es2021: true,
+    es2021: true
   },
   // extends: 'airbnb-base',
   extends: 'eslint:recommended',
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 'latest'
   },
   // plugins: ['import'],
   globals: {
     var1: 'writeable',
-    Var2: 'readonly',
+    Var2: 'readonly'
   },
   rules: {
     'comma-dangle': 1,
     'Linebreak-style': 0,
     'no-unused-vars': 0,
-    'max-len': 0,
-  },
+    'max-len': 0
+  }
+  // 'comma-dangle': ['error', 'always'],
 }
